6a309ed2ac86283065610267a60829f79cc4d3dc
Dharmambal <dh793620@dal.ca>
2019-02-25 22:31:49 -0400

    Committing sudokufile

---
+++ Main.java
@@ -0,0 +1,106 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		int gridsize=getsize();
+		char[][] matrix=new char[gridsize][gridsize];
+		char[] symbols=getsymbols(gridsize);
+		getmatrix(gridsize,symbols,matrix);
+		Sudoku sd=new Sudoku(matrix,gridsize,symbols);
+		boolean solution=sd.sudokusolution();
+		if(solution)
+		{
+			System.out.println("sudoku solution");
+			sd.display();
+		}
+		else
+		{
+			System.out.println("Unsolvable");
+		}
+	}
+
+	private static void getmatrix(int gridsize, char[] symbols, char[][] matrix) 
+	{
+		// TODO Auto-generated method stub
+		String grid="";
+		int counter=0;
+		System.out.println("Enter the sudoku grid to be solved:");
+		Scanner input=new Scanner(new BufferedInputStream(System.in));
+		while (counter<gridsize && input.hasNext() )
+		{
+			String line=input.next();
+			buildmatrix(line,gridsize,counter,matrix);
+			counter++;
+		}
+	}
+
+	private static void buildmatrix(String line, int gridsize, int counter, char[][] matrix) {
+		// TODO Auto-generated method stub
+		char[] array = new char[line.length()];
+		int i = 0;
+		for (char c : line.toCharArray())
+		{
+			if(c=='.')
+			{
+				array[i++]='0';
+			}
+			else
+			{
+				array[i++] = c;	
+			}
+		}
+		if(array.length==0)
+		{
+			System.out.println("Input cannot be empty");
+		}
+		else
+		{
+			for (int j = 0; j < gridsize; j++)
+			{
+				matrix[counter][j] = array[j];
+			}
+		}
+
+	}
+
+	private static char[] getsymbols(int gridsize) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Enter the list of Symbols:");
+		Scanner input=new Scanner(System.in);
+		String symbols=input.next();
+		char[] array = new char[symbols.length()];
+		int i = 0;
+		for (char c : symbols.toCharArray())
+			array[i++] = c;
+		if(array.length!=gridsize)
+		{
+			System.out.println("Number of symbols are not correct");
+			getsymbols(gridsize);
+		}
+		else if(array.length==0)
+		{
+			System.out.println("Symbols cannot be null");
+		}
+		return array;
+	}
+
+	private static int getsize()
+	{
+
+		boolean intvalue;
+		System.out.println("Enter the size of the grid: ");
+		Scanner input=new Scanner(System.in);
+		int size=input.nextInt();
+		if(size==0)
+		{
+			System.out.println("Size cannot be null!!");
+			getsize();
+		}
+		return size;
+	}
+
+}
---
+++ Sudoku.java
@@ -0,0 +1,90 @@
+/* Reference: GeekstoGeeks.com (for logic)*/
+public class Sudoku {
+	public char[][] sudokugrid;
+	public int gridsize;
+	public char[] symbolsArr;
+
+	public Sudoku(char[][] matrix, int size, char[] symbolsArr) {
+		// TODO Auto-generated constructor stub
+		this.gridsize = size;
+		this.sudokugrid = new char[size][size];
+		this.symbolsArr = symbolsArr;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				this.sudokugrid[i][j] = matrix[i][j];
+			}
+		}
+	}
+	public boolean sudokusolution() {
+
+		// TODO Auto-generated method stub
+		for (int i = 0; i < gridsize; i++) {
+			for (int j = 0; j < gridsize; j++) {
+				if (sudokugrid[i][j] == '0') {
+					for (int k = 0; i < symbolsArr.length; k++) {
+						boolean presentinrow = inRow(i, symbolsArr[k], gridsize);
+						boolean presentincolumn = inColumn(j, symbolsArr[k], gridsize);
+						boolean presentinsmallgrid = inSmallgrid(i, j, symbolsArr[k]);
+						if (presentinrow == false && presentincolumn == false && presentinsmallgrid == false) {
+							sudokugrid[i][j] = symbolsArr[k];
+							if (sudokusolution()) {
+								return true;
+							} else {
+								sudokugrid[i][j] = '0';
+							}
+						}
+					}
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private boolean inSmallgrid(int row, int column, char symbol) {
+		// TODO Auto-generated method stub
+		int sgrow = row - (row % 3);
+		int sgcolumn = column - (column % 3);
+		for (int i = sgrow; i < sgrow; i++) {
+			for (int j = sgcolumn; j < sgcolumn; j++) {
+				if (sudokugrid[sgrow][sgcolumn] == symbol) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private boolean inColumn(int column, char symbol, int size) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < size; i++) {
+			if (sudokugrid[i][column] == symbol) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean inRow(int row, char symbol, int size) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < size; i++) {
+			if (sudokugrid[row][i] == symbol) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void display() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < gridsize; i++) {
+			for (int j = 0; j < gridsize; j++) {
+				System.out.print(" " + sudokugrid[i][j]);
+			}
+
+			System.out.println();
+		}
+		System.out.println();
+	};
+
+}
